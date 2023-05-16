bef6de07d257d46e34fccb56a73a93be4e61318d
javie <javie@LAPTOP-68FO9DBP>
2023-05-16 11:05:55 +0200

    Meto la suma de dos numeros

--- examenDefinitivo/src/examenDefinitivo/ExamenDefinitivo.java
+++ examenDefinitivo/src/examenDefinitivo/ExamenDefinitivo.java
@@ -3,7 +3,9 @@ package examenDefinitivo;
 public class ExamenDefinitivo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a=14214;
+		int b=142151;		
+		System.out.println("Esta es la suma de los dos numeros: " + (a + b));
 
 	}
 
