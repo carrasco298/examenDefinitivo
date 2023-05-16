62efc6e420ae78daeb1d30fe00926b779f957979
javie <javie@LAPTOP-68FO9DBP>
2023-05-16 10:57:19 +0200

    EL EXAMEN DEFINITIVO

---
+++ examenDefinitivo/src/examenDefinitivo/ExamenDefinitivo.java
@@ -0,0 +1,10 @@
+package examenDefinitivo;
+
+public class ExamenDefinitivo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ examenDefinitivo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author javie
+ *
+ */
+module examenDefinitivo {
+}
